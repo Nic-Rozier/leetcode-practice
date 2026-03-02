f3bb0ab669f2ea4297d3c8404df79ac3aa36cd86
Nic-Rozier <nicolasprozier@gmail.com>
2026-03-02 11:49:19 -0600

    Added ValidAnagram.py

--- Arrays & Hashing/ValidAnagram.py
+++ Arrays & Hashing/ValidAnagram.py
@@ -5,3 +5,8 @@ class Solution:
     '''
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
+
+    '''
+    More efficient space wise than a hash map, but
+    has to run the sorting algorithm.
+    '''
